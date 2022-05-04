14a67ebb8c912c7aec396d23f2d78b18f338203d
prathapkrishnagithub <90118784+prathapkrishnagithub@users.noreply.github.com>
2022-05-05 00:02:19 +0530

    Add files via upload

---
+++ src/pages/Aboutpage.js
@@ -0,0 +1,21 @@
+const Aboutpage = () => {
+    return (
+      <div className="about">
+        <h3 className="about__title">ABOUT PAGE</h3>
+        <div className="about__info">
+
+
+
+
+          <h1>Internship Test
+ReactJS Developer Profile Set-2
+
+ </h1>
+
+        </div>
+      </div>
+    );
+  };
+  
+  export default Aboutpage;
+  
